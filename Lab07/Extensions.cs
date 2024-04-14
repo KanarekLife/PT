0a5706014ea8234faf8b71990af8025bf6383047
Stanisław Nieradko <stanislaw@nieradko.com>
2024-04-14 13:00:13 +0200

    add Lab07

---
+++ Lab07/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Lab07
+{
+    public static class Extensions
+    {
+        public static FileInfo GetEldestElement(this DirectoryInfo directoryInfo)
+        {
+            return directoryInfo
+                .EnumerateDirectories()
+                .Select(x => x.GetEldestElement())
+                .Concat(directoryInfo.EnumerateFiles())
+                .OrderBy(x => x.CreationTimeUtc)
+                .First();
+        }
+
+        public static string GetDosAttributes(this FileSystemInfo fileSystemInfo)
+        {
+            var attributes = fileSystemInfo.Attributes;
+            var dosAttributes = new StringBuilder();
+            dosAttributes.Append((attributes & FileAttributes.ReadOnly) != 0 ? 'R' : '-');
+            dosAttributes.Append((attributes & FileAttributes.Archive) != 0 ? 'A' : '-');
+            dosAttributes.Append((attributes & FileAttributes.Hidden) != 0 ? 'H' : '-');
+            dosAttributes.Append((attributes & FileAttributes.System) != 0 ? 'S' : '-');
+            return dosAttributes.ToString();
+        }
+    }
+}
---
+++ Lab07/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Lab07
+{
+    internal class Program
+    {
+        private static void PrintDirectory(DirectoryInfo directory, string indent = "")
+        {
+            const string indentationLevel = "  ";
+
+            var subDirs = directory.EnumerateDirectories()
+                .ToArray();
+            var subFiles = directory.EnumerateFiles()
+                .ToArray();
+            
+            Console.WriteLine($"{indent}{directory.Name}/\t{subDirs.Length + subFiles.Length} elements");
+            
+            foreach (var dir in subDirs)
+            {
+                PrintDirectory(dir, indent + indentationLevel);
+            }
+            
+            foreach (var file in subFiles)
+            {
+                Console.WriteLine($"{indent}{indentationLevel}{file.Name}\t{file.GetDosAttributes()}\t{file.Length}B");
+            }
+        }
+        
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("Usage: Lab07 <directory>");
+                return;
+            }
+            
+            var directory = new DirectoryInfo(Path.GetFullPath(args[0]));
+            PrintDirectory(directory);
+            
+            Console.WriteLine();
+            
+            var a = directory.EnumerateFiles().Select(x => (x.Name, x.Length));
+            var b = directory.EnumerateDirectories().Select(x => (x.Name, x.EnumerateDirectories().Count() + x.EnumerateFiles().LongCount()));
+            var collection = a.Concat(b)
+                .OrderBy(x => x.Name.Length)
+                .ThenBy(x => x.Name)
+                .ToDictionary(x => x.Name, x => x.Item2);
+
+            using (var ms = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(ms, collection);
+
+                ms.Position = 0;
+
+                var tmp = formatter.Deserialize(ms) as Dictionary<string, long>;
+                foreach (var pair in tmp)
+                {
+                    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+                }
+            }
+        }
+    }
+}
